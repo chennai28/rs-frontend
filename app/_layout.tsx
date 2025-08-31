01ab2df1afc035257ee3f89dff0160dbc2cd185e
Varsha <vvarsha1020@gmail.com>
2025-08-31 13:07:24 -0700

    another merge conflict

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { JetBrainsMono_700Bold, useFonts } from "@expo-google-fonts/jetbrains-mono";
 import AppLoading from "expo-app-loading";
 import { Slot } from "expo-router";
@@ -13,34 +12,4 @@ export default function Layout() {
   }
 
   return <Slot />;
-=======
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
->>>>>>> ef25bfdabd706f857cdfeb86fb3242632db1aa38
 }
